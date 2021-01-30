c745d968643a4832cca2c17aa36a811213f925d4
brendan sudol <brendansudol@gmail.com>
2021-01-29 22:00:06 -0500

    simpler email validation

--- utils/email.ts
+++ utils/email.ts
@@ -1,4 +1,3 @@
 export function isEmailValid(email: string): boolean {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }

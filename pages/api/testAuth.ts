86075e56aa0b47f19bcc082ee0fbf0f9bf168f3e
brendan sudol <brendansudol@gmail.com>
2021-02-14 22:09:16 -0500

    add console log

--- pages/api/testAuth.ts
+++ pages/api/testAuth.ts
@@ -2,5 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const auth = req.headers.authorization ?? "N/A";
+  console.log(`auth = ${auth}`);
   return res.status(200).json({ auth });
 };

f0ea9724d65c589cfa6bebd7eeaa18ab6eaba44b
brendan sudol <brendansudol@gmail.com>
2021-02-15 10:01:09 -0500

    auth update

--- pages/api/testAuth.ts
+++ pages/api/testAuth.ts
@@ -1,7 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const auth = req.headers.authorization ?? "N/A";
-  console.log(`auth = ${auth}`);
-  return res.status(200).json({ auth });
+  const auth = getAuthToken(req);
+
+  let status: string | null = null;
+  if (auth == null) status = "none";
+  else if (auth === process.env.SMARTFUL_AUTH_KEY) status = "success";
+  else status = "fail";
+
+  console.log(`auth status = ${status}`);
+  return res.status(200).json({ auth: status });
 };
+
+function getAuthToken(req: NextApiRequest): string | undefined {
+  return req.headers.authorization?.slice(7) || undefined;
+}
